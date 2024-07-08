9f8fb52126362266a17ca287bc8b5d5cc42e3337
Akshay Rathee <akshayrathee902@gmail.com>
2024-07-08 14:14:47 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import cors from "cors";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import path from "path";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+
+// app.use("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`server runing on ${process.env.DEV_MODE} mode on ${PORT}`);
+});
